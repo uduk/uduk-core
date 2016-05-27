d4c6882b6f3276b3ffec436d848c3903840b1601
uduk <iherlambang@gmail.com>
2016-05-27 22:36:18 +0700

    Add files via upload

--- sequence.js
+++ sequence.js
@@ -52,6 +52,40 @@ var UdukSequence = {
     return interval;
   },
 
+  toMIDINote: function(s, f) {
+    var six  =    [ "E2", "F2", "F#2", "G2", "G#2", "A2", "A#2", "B2", "C3", "C#3", "D3", "D#3",
+                    "E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3", "C4", "C#4", "D4", "D#4", 
+                    "E4"];
+    var five  =   [ "A2", "A#2", "B2", "C3", "C#3", "D3", "D#3", "E3", "F3", "F#3", "G3", "G#3", 
+                    "A3", "A#3", "B3", "C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4",
+                    "A4"];
+    var four  =   [ "D3", "D#3", "E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3", "C4", "C#4", 
+                    "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4", "C5", "C#5",
+                    "D5"];
+    var three =   [ "G3", "G#3", "A3", "A#3", "B3", "C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", 
+                    "G4", "G#4", "A4", "A#4", "B4", "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", 
+                    "G5"];
+    var two   =   [ "B3", "C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", 
+                    "B4", "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", 
+                    "B5"];
+    var one   =   [ "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4", "C5", "C#5", "D5", "D#5", 
+                    "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5", "C6", "C#6", "D6", "D#6",
+                    "E6"];
+
+    var midiNoteTable = [];
+        midiNoteTable.push(["0"]);
+        midiNoteTable.push(one); 
+        midiNoteTable.push(two);
+        midiNoteTable.push(three);
+        midiNoteTable.push(four);
+        midiNoteTable.push(five);
+        midiNoteTable.push(six);
+
+    s = parseInt(s);
+    f = parseInt(f);
+    return midiNoteTable[s][f];
+  },
+
   splitNote: function(note)
   {
     var x = note.toString();
