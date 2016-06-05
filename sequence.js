9c5726b0603e09f5311aae340f14f4b505a32213
uduk <iherlambang@gmail.com>
2016-06-06 05:42:05 +0700

    Add files via upload

--- sequence.js
+++ sequence.js
@@ -52,7 +52,18 @@ var UdukSequence = {
     return interval;
   },
 
-  toMIDINote: function(s, f) {
+  toMIDI: function (sequence) 
+  {
+    var buildSequence = [];
+    for (var i = 0; i < sequence.length; i++) {
+      var s = UdukSequence.splitNote(sequence[i]);
+      buildSequence.push(UdukSequence.toMIDINote(s[0], s[1]));
+    }
+    return buildSequence; 
+  },
+
+  toMIDINote: function(s, f) 
+  {
     var six   = [ "E2", "F2", "F#2", "G2", "G#2", "A2", "A#2", "B2", "C3", "C#3", "D3", "D#3", "E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3", "C4", "C#4", "D4", "D#4", "E4"];
     var five  = [ "A2", "A#2", "B2", "C3", "C#3", "D3", "D#3", "E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3", "C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4"];
     var four  = [ "D3", "D#3", "E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3", "C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4", "C5", "C#5", "D5"];
